8112bcf6ef47259b5244cfb0ca3e0172c2048d20
Anch <adrianpratt.ap@gmail.com>
2025-01-08 00:19:38 +1300

    replaced settingscreater lib

--- ExtendedVendorUI/ExtendedVendorUI.lua
+++ ExtendedVendorUI/ExtendedVendorUI.lua
@@ -10,7 +10,7 @@ EV.worldFrameHook = {}
 
 --Set Savedvariables defaults
 local DefaultSettings  = {
-    TxtSize         = 12,
+    TxtSize = 12,
     WhiteList = {},
     BlackList = {},
     AutoVendor = false,
